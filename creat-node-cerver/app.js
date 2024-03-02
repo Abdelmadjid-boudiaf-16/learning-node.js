a4ad1244de2327d897ebbe534a7a2d6ca48309f3
abdelmadjid-boudiaf-16 <boudiafabdelmadjid16@gmail.com>
2024-03-02 15:16:25 +0100

    first commit

---
+++ creat-node-cerver/app.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>test server</title></head><body><form action='/message' method='POST'><input type='text' name='test'/><button>send</button></form></body></html>"
+    );
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+});
+
+server.listen(3000);
+
+/* 
+(req, res) => {
+  //   console.log(req.url, req.method, req.headers);
+  // process.exit()  end the server
+
+  //send response
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      "<html><head><title>test server!</title><body><form action='/message' method='POST'><input type='text' name='test' /><button>send</button></form></body></head></html > "
+    );
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split("=")[1];
+      fs.writeFileSync("test.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+}
+*/
